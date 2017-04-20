7a660988451bf043ed7cd69cb0e34780f3854911
alex <alex@192.168.0.227>
2017-04-20 18:29:06 +0100

    REST controller tests enhanced.

--- src/test/java/com/gav/quiz/strider/controller/StriderMainControllerTest.java
+++ src/test/java/com/gav/quiz/strider/controller/StriderMainControllerTest.java
@@ -1,10 +1,20 @@
 package com.gav.quiz.strider.controller;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import java.nio.charset.Charset;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -12,10 +22,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.gav.quiz.strider.StriderApplication;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 /**
  * @author alex.gera
@@ -27,6 +33,10 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 public class StriderMainControllerTest {
 	@Autowired
     private WebApplicationContext webApplicationContext;
+	
+	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
 
 	private MockMvc mockMvc;
 
@@ -34,6 +44,8 @@ public class StriderMainControllerTest {
     public void setup() throws Exception {
         this.mockMvc = webAppContextSetup(webApplicationContext).build();
     }
+	
+	/////////Params acceptance checks:
 
 	 @Test
 	 public void testAppNameOnly_badRequest() throws Exception {
@@ -84,5 +96,78 @@ public class StriderMainControllerTest {
 	        		.param("stairwell", "1,2,3")
 	        		.param("extra-param", "extra"))
                  .andExpect(status().isOk());
+	 }
+	 
+	 /////////Content checks by example:
+	 
+	 @Test
+	 public void testContent_ExampleOne() throws Exception {
+		mockMvc.perform(get("/strider")
+	        		.param("sps", "3")
+	        		.param("stairwell", "17"))
+                 .andExpect(status().isOk())
+                 .andExpect(content().contentType(contentType))
+                 .andExpect(jsonPath("$.minimumStridesAmount", is(6)));
+	 }
+	 
+	 @Test
+	 public void testContent_ExampleTwo() throws Exception {
+		mockMvc.perform(get("/strider")
+	        		.param("sps", "3")
+	        		.param("stairwell", "17,17"))
+                 .andExpect(status().isOk())
+                 .andExpect(content().contentType(contentType))
+                 .andExpect(jsonPath("$.minimumStridesAmount", is(14)));
+	 }
+	 
+	 @Test
+	 public void testContent_ExampleThree() throws Exception {
+		mockMvc.perform(get("/strider")
+	        		.param("sps", "2")
+	        		.param("stairwell", "4,9,8,11,7,20,14"))
+                 .andExpect(status().isOk())
+                 .andExpect(content().contentType(contentType))
+                 .andExpect(jsonPath("$.minimumStridesAmount", is(50)));
+	 }
+	 
+	 /////////Content checks in invalid requests:
+	 @Test
+	 public void testContent_InvalidZeroSps() throws Exception {
+		mockMvc.perform(get("/strider")
+	        		.param("sps", "0")
+	        		.param("stairwell", "4,9,8,11,7,20,14"))
+                 .andExpect(status().isOk())
+                 .andExpect(content().contentType(contentType))
+                 .andExpect(jsonPath("$.minimumStridesAmount", is(0)));
+	 }
+	 
+	 @Test
+	 public void testContent_InvalidNegativeSps() throws Exception {
+		mockMvc.perform(get("/strider")
+	        		.param("sps", "-3")
+	        		.param("stairwell", "4,9,8,11,7,20,14"))
+              .andExpect(status().isOk())
+              .andExpect(content().contentType(contentType))
+              .andExpect(jsonPath("$.minimumStridesAmount", is(0)));
+	 }
+	 
+	 @Test
+	 public void testContent_InvalidZeroInStairwell() throws Exception {
+		mockMvc.perform(get("/strider")
+	        		.param("sps", "2")
+	        		.param("stairwell", "4,9,8,0,7,20,14"))
+              .andExpect(status().isOk())
+              .andExpect(content().contentType(contentType))
+              .andExpect(jsonPath("$.minimumStridesAmount", is(0)));
+	 }
+	 
+	 @Test
+	 public void testContent_InvalidNegativeInStairwell() throws Exception {
+		mockMvc.perform(get("/strider")
+	        		.param("sps", "-3")
+	        		.param("stairwell", "4,9,8,-11,7,20,14"))
+              .andExpect(status().isOk())
+              .andExpect(content().contentType(contentType))
+              .andExpect(jsonPath("$.minimumStridesAmount", is(0)));
 	 }
 }
